ffd22edddc45f52d76090b6d25c44785089d2f03
Antavio <njuguna13@gmail.com>
2019-05-10 17:49:34 +0300

    Connect our application to postgres database and to make migratios.

--- pic_galore/settings.py
+++ pic_galore/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'pic_galore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'pic_galore',
+        'USER': 'antavio',
+        'PASSWORD': 'antavio',
     }
 }
 
@@ -120,3 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
