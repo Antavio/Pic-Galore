9bc506ae2db7abd3aa6e33c8deb8228c4f9f8a6a
Antavio <njuguna13@gmail.com>
2019-05-11 10:31:01 +0300

    Create a Test case for Category Model Class to confirm if the object is instanciated and saved  correctly.

--- collage/models.py
+++ collage/models.py
@@ -7,12 +7,18 @@ class Location(models.Model):
     def __str__(self):
         return self.location
 
+    def save_location(self):
+        self.save()
+
 class Category(models.Model):
     category = models.CharField(max_length=50)
 
     def __str__(self):
         return self.category
-        
+
+    def save_category(self):
+        self.save()
+
 class Image(models.Model):
     image_path = models.ImageField(upload_to = 'images/',default="")
     image_name = models.CharField(max_length=50)
--- collage/tests.py
+++ collage/tests.py
@@ -7,3 +7,19 @@ class LocationTestClass(TestCase):
         
     def test_instance(self):
         self.assertTrue(isinstance(self.mombasa,Location))
+    def test_save_method(self):
+        self.mombasa.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations)>0)
+
+class CategoryTestClass(TestCase):
+    def setUp(self):
+        self.pets = Category(category='Animals')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.pets,Category))
+
+    def test_save_method(self):
+        self.pets.save_category()
+        categories = Category.objects.all()
+        self.assertTrue(len(categories)>0)
