f5dd9cb98b3bc160c9b149f09f5d57e6de8050c2
Antavio <njuguna13@gmail.com>
2019-05-11 10:13:06 +0300

    Create a Test case for Location Model Class to confirm if the object is instanciated correctly.

--- collage/tests.py
+++ collage/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from .models import Location,Category,Image
 
-# Create your tests here.
+class LocationTestClass(TestCase):
+    def setUp(self):
+        self.mombasa = Location(location='Mombasa')
+        
+    def test_instance(self):
+        self.assertTrue(isinstance(self.mombasa,Location))
